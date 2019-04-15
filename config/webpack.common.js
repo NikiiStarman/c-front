8876aaf3eec5938baeb65426ad8d9545f8710410
NikiiStarman <janis.niklass@gmail.com>
2019-04-15 17:57:17 +0300

    webpack update

--- config/webpack.common.js
+++ config/webpack.common.js
@@ -1,46 +1,51 @@
-const webpack = require('webpack');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = {
-    entry: {
-        'vendor': './src/vendor.js',
-        'app': './src/main.jsx'
-    },
+  entry: {
+    vendor: "./src/vendor.js",
+    app: "./src/main.jsx"
+  },
 
-    resolve: {
-        extensions: ['.jsx', '.js']
-    },
+  resolve: {
+    extensions: [".jsx", ".js"]
+  },
 
-    module: {
-        rules: [
-            {
-                test: /\.jsx?$/,
-                exclude: /node_modules/,
-                loader: 'babel-loader',
-                query: { presets: ['react', 'env'] },
-            },
-            {
-                test: /\.ico$$/,
-                loader: 'file-loader?name=[name].[ext]'
-            },
-            {
-                test: /\.scss$/,
-                use: ExtractTextPlugin.extract({
-                    fallback: 'style-loader',
-                    use: ['css-loader?minimize', 'sass-loader']
-                })
-            }
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        loader: "babel-loader",
+        query: { presets: ["react", "env"] }
+      },
+      {
+        test: /\.ico$$/,
+        loader: "file-loader?name=[name].[ext]"
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          { loader: MiniCssExtractPlugin.loader },
+          "css-loader?minimize",
+          "sass-loader"
         ]
-    },
-
-    plugins: [
-        new webpack.optimize.CommonsChunkPlugin({
-            name: ['app', 'vendor']
-        }),
-
-        new HtmlWebpackPlugin({
-            template: 'src/index.html'
-        })
+      }
     ]
+  },
+
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "src/index.html"
+    }),
+    new MiniCssExtractPlugin({
+      filename: "[name].css",
+      chunkFilename: "[id].css"
+    })
+  ],
+  optimization: {
+    splitChunks: {
+      chunks: "all"
+    }
+  }
 };
--- config/webpack.dev.js
+++ config/webpack.dev.js
@@ -1,24 +1,27 @@
-const webpackMerge = require('webpack-merge');
-const ExtractTextPlugin = require('extract-text-webpack-plugin');
-const commonConfig = require('./webpack.common.js');
-const helpers = require('./helpers');
+const webpackMerge = require("webpack-merge");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const commonConfig = require("./webpack.common.js");
+const helpers = require("./helpers");
 
 module.exports = webpackMerge(commonConfig, {
-    devtool: 'cheap-module-eval-source-map',
+  devtool: "cheap-module-eval-source-map",
 
-    output: {
-        path: helpers.root('dist'),
-        publicPath: 'http://localhost:8080/',
-        filename: '[name].js',
-        chunkFilename: '[id].chunk.js'
-    },
+  output: {
+    path: helpers.root("dist"),
+    publicPath: "http://localhost:8080/",
+    filename: "[name].js",
+    chunkFilename: "[id].chunk.js"
+  },
 
-    plugins: [
-        new ExtractTextPlugin('[name].css')
-    ],
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: "[name].css",
+      chunkFilename: "[id].css"
+    })
+  ],
 
-    devServer: {
-        historyApiFallback: true,
-        stats: 'minimal'
-    }
+  devServer: {
+    historyApiFallback: true,
+    stats: "minimal"
+  }
 });
